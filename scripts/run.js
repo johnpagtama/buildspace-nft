96b9109b960314aef705822daacef45eda15643e
johnpagtama <pagtamajr@gmail.com>
2022-08-06 11:16:35 -0700

    Deployed NFT contract on localhost

---
+++ scripts/run.js
@@ -0,0 +1,25 @@
+const main = async () => {
+	const nftContractFactory = await hre.ethers.getContractFactory(
+		'MyEpicNFT'
+	);
+
+	const nftContract = await nftContractFactory.deploy();
+
+	await nftContract.deployed();
+
+	console.log('Contract deployed to: ', nftContract.address);
+};
+
+const runMain = async () => {
+	try {
+		await main();
+
+		process.exit(0);
+	} catch (err) {
+		console.error(err);
+
+		process.exit(1);
+	}
+};
+
+runMain();
